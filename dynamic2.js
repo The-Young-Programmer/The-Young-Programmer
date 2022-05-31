56d42ba1c96846403578ba8c91084ff221be513b
Nemonet <79866006+The-Young-Programmer@users.noreply.github.com>
2022-05-31 01:04:14 +0100

    Delete dynamic2.js

--- dynamic2.js
+++
@@ -1,6 +0,0 @@
-var sec1__menuIcon = document.querySelector(".sec1__menuIcon");
-      var menuBar = document.querySelector(".menuBar");
-      sec1__menuIcon.addEventListener("click", function () {
-        sec1__menuIcon.classList.toggle("active");
-        menuBar.classList.toggle("active");
-      });
